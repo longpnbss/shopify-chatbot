cf3417001ea2520445fe92574293918fe7986233
longpn <longpn@bsscommerce.com>
2025-04-03 10:05:09 +0700

    handle help center screen, function step

--- extensions/chatbot-ui/assets/help-center.js
+++ extensions/chatbot-ui/assets/help-center.js
@@ -1,1 +1,25 @@
-document.addEventListener("DOMContentLoaded", function () {});
+// List step
+const HELP_CENTER = "help-center";
+const FAQS = "faqs";
+const SUGGESTION = "suggestion";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const helpCenterOptions = document.querySelectorAll(
+    ".chatbot-help-center-option"
+  );
+
+  function showStep(step) {
+    document
+      .querySelectorAll(".chatbot-step")
+      .forEach((el) => el.classList.remove("active"));
+    document.getElementById("chatbot-step-" + step).classList.add("active");
+  }
+
+  helpCenterOptions.forEach((option) => {
+    option.addEventListener("click", () => {
+      showStep(option.id);
+    });
+  });
+
+  showStep(HELP_CENTER);
+});
